f51761d8698d8ec26ef0f74d467d2d7e57b2c347
Yanglin Ma <lima@microsoft.com>
2016-04-25 20:55:47 +0800

    initiazlie

---
+++ DesignPatterns/behavioral/Interpreter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.behavioral.InterpreterApp
+{
+    /// <summary>
+    /// MainApp startup class for Structural 
+    /// Interpreter Design Pattern.
+    /// </summary>
+    class InterpreterApp
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Show()
+        {
+            Context context = new Context();
+
+            // Usually a tree 
+            List<AbstractExpression> list = new List<AbstractExpression>();
+
+            // Populate 'abstract syntax tree' 
+            list.Add(new TerminalExpression());
+            list.Add(new NonterminalExpression());
+            list.Add(new TerminalExpression());
+            list.Add(new TerminalExpression());
+
+            // Interpret
+            foreach (AbstractExpression exp in list)
+            {
+                exp.Interpret(context);
+            }
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Context' class
+    /// </summary>
+    class Context
+    {
+    }
+
+    /// <summary>
+    /// The 'AbstractExpression' abstract class
+    /// </summary>
+    abstract class AbstractExpression
+    {
+        public abstract void Interpret(Context context);
+    }
+
+    /// <summary>
+    /// The 'TerminalExpression' class
+    /// </summary>
+    class TerminalExpression : AbstractExpression
+    {
+        public override void Interpret(Context context)
+        {
+            Console.WriteLine("Called Terminal.Interpret()");
+        }
+    }
+
+    /// <summary>
+    /// The 'NonterminalExpression' class
+    /// </summary>
+    class NonterminalExpression : AbstractExpression
+    {
+        public override void Interpret(Context context)
+        {
+            Console.WriteLine("Called Nonterminal.Interpret()");
+        }
+    }
+}
---
+++ DesignPatterns/structural/Flyweight.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.structural.FlyweightApp
+{
+    /// <summary>
+    /// MainApp startup class for Structural 
+    /// Flyweight Design Pattern.
+    /// </summary>
+    class FlyweightApp
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Show()
+        {
+            // Arbitrary extrinsic state
+            int extrinsicstate = 22;
+
+            FlyweightFactory factory = new FlyweightFactory();
+
+            // Work with different flyweight instances
+            Flyweight fx = factory.GetFlyweight("X");
+            fx.Operation(--extrinsicstate);
+
+            Flyweight fy = factory.GetFlyweight("Y");
+            fy.Operation(--extrinsicstate);
+
+            Flyweight fz = factory.GetFlyweight("Z");
+            fz.Operation(--extrinsicstate);
+
+            UnsharedConcreteFlyweight fu = new
+              UnsharedConcreteFlyweight();
+
+            fu.Operation(--extrinsicstate);
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'FlyweightFactory' class
+    /// </summary>
+    class FlyweightFactory
+    {
+        private Dictionary<string, Flyweight> flyweights = new Dictionary<string, Flyweight>();
+
+        // Constructor
+        public FlyweightFactory()
+        {
+            flyweights.Add("X", new ConcreteFlyweight());
+            flyweights.Add("Y", new ConcreteFlyweight());
+            flyweights.Add("Z", new ConcreteFlyweight());
+        }
+
+        public Flyweight GetFlyweight(string key)
+        {
+            return ((Flyweight)flyweights[key]);
+        }
+    }
+
+    /// <summary>
+    /// The 'Flyweight' abstract class
+    /// </summary>
+    abstract class Flyweight
+    {
+        public abstract void Operation(int extrinsicstate);
+    }
+
+    /// <summary>
+    /// The 'ConcreteFlyweight' class
+    /// </summary>
+    class ConcreteFlyweight : Flyweight
+    {
+        public override void Operation(int extrinsicstate)
+        {
+            Console.WriteLine("ConcreteFlyweight: " + extrinsicstate);
+        }
+    }
+
+    /// <summary>
+    /// The 'UnsharedConcreteFlyweight' class
+    /// </summary>
+    class UnsharedConcreteFlyweight : Flyweight
+    {
+        public override void Operation(int extrinsicstate)
+        {
+            Console.WriteLine("UnsharedConcreteFlyweight: " +
+              extrinsicstate);
+        }
+    }
+}
